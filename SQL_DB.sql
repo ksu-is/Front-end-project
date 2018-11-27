75b156afed94ee895ee4d645d21980ac6285858d
Anderbee <amitch95@students.kennesaw.edu>
2018-11-27 09:48:10 -0500

    Update SQL_DB.sql

--- SQL_DB.sql
+++ SQL_DB.sql
@@ -2,15 +2,6 @@ create database if not exists TheDB;
 
 use TheDB;
 
-CREATE TABLE IF NOT EXISTS TheTable (
-    `index` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `key` VARCHAR(255) NOT NULL,
-    `value` VARCHAR(255) NOT NULL
+CREATE TABLE GRADES (
+StudentID char(255),
 );
-    
-drop table TheTable;
-
-SELECT 
-    *
-FROM
-    TheTable;
